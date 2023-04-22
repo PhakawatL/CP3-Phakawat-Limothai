e31d4e70321a7390b134662194a5b8bad0214f0c
PhakawatL <ipeemy@gmail.com>
2023-04-22 16:03:16 +0700

    Exercise4

---
+++ Exercise4_Phakawat_L.py
@@ -0,0 +1,13 @@
+โจทย์ = "โจทย์ : ให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียน"
+print(โจทย์)
+
+Sccore_FoundationEnglish = 65.5
+Sccore_GeneralBusiness = 86
+Sccore_IntroductiontoComputerSystems = 71.5
+Sccore_ComputerProgramming = 81
+
+print("--- Your Score ---")
+print("Foundation English               :",Sccore_FoundationEnglish)
+print("General Business                 :",Sccore_GeneralBusiness)
+print("Introduction to Computer Systems :",Sccore_IntroductiontoComputerSystems)
+print("Computer Programming             :",Sccore_ComputerProgramming)
